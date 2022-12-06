88e12a50026fe3e6f59789b73637fb50262e4ea8
pablocw13 <119915529+luis5421u@users.noreply.github.com>
2022-12-05 23:04:02 -0600

    Initial commit

---
+++ app/src/main/java/camberos/hernandez/criminalintent/CriminalIntentApplication.kt
@@ -0,0 +1,10 @@
+package camberos.hernandez.criminalintent
+
+import android.app.Application
+
+class CriminalIntentApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CrimeRepository.initialize(this)
+    }
+}
